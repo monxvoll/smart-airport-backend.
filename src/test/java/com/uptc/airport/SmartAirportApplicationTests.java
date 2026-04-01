af841d0d529742b2465a16bc19a7c96bfda651b2
monxvll <stedy3348112688@gmail.com>
2026-04-01 18:48:59 -0500

    chore(setup): track maven wrapper and test directory

---
+++ src/test/java/com/uptc/airport/SmartAirportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAirportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
